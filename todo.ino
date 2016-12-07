ec4107290a4d60ecba80ec3031d08dc48fa2f868
YQN <spam-me@yann-g.net>
2016-12-07 17:12:42 +0100

    Add files via upload

--- todo.ino
+++ todo.ino
@@ -2,13 +2,13 @@
  * mandatory
  */
 // DONE check if game over
+// reset when game over
 // display other player's shots
 // sound when hit
 // make sinking anim play even if button pressed before the end of the anim
 // draw sunk enemy boats bitmaps
 // update mini map
 // anim when game over
-// menu
 
 /*
  * cosmetics
@@ -16,3 +16,4 @@
 // anim when boat hit
 // anim when boat missed
 // missile sound
+// menu
